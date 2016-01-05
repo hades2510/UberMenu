c54a8767d03d6684e20c06f7fbe832d1e6faf3dd
Radu Dita <hades2510@yahoo.com>
2016-01-05 19:03:10 +0200

    added tag class

---
+++ UberMenu/UBTag.swift
@@ -0,0 +1,13 @@
+//
+//  UBTag.swift
+//  UberMenu
+//
+//  Created by Mac on 05/01/16.
+//  Copyright © 2016 samuraibonzai. All rights reserved.
+//
+
+import Foundation
+
+struct UBTag{
+    let name:String
+}
