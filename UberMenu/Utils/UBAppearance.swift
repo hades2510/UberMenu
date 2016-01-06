8c0ffc0e55f8251f74daed0904073873ac148299
Radu Dita <hades2510@yahoo.com>
2016-01-06 18:56:18 +0200

    added support for UIAppearance

---
+++ UberMenu/Utils/UBAppearance.swift
@@ -0,0 +1,19 @@
+//
+//  UBAppearance.swift
+//  UberMenu
+//
+//  Created by Mac on 06/01/16.
+//  Copyright © 2016 samuraibonzai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UBAppearance{
+    
+    static func setAppearance(){
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor(red: 68/255.0, green: 68/255.0, blue: 68/255.0, alpha: 1.0)]
+        UINavigationBar.appearance().tintColor = UIColor(red: 68/255.0, green: 68/255.0, blue: 68/255.0, alpha: 1.0)
+    }
+    
+}
