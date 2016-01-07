13d4959444ebecb793e2eb7504f0f349ebdb2c14
Radu Dita <hades2510@yahoo.com>
2016-01-07 17:45:27 +0200

    tint colour for activity indicators

--- UberMenu/Utils/UBAppearance.swift
+++ UberMenu/Utils/UBAppearance.swift
@@ -14,6 +14,7 @@ class UBAppearance{
     static func setAppearance(){
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor(red: 68/255.0, green: 68/255.0, blue: 68/255.0, alpha: 1.0)]
         UINavigationBar.appearance().tintColor = UIColor(red: 68/255.0, green: 68/255.0, blue: 68/255.0, alpha: 1.0)
+        UIActivityIndicatorView.appearance().tintColor = UIColor(red: 123/255.0, green: 229/255.0, blue: 60/255.0, alpha: 1.0)
     }
     
 }
