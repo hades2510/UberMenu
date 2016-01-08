eb424ff2753982904c42afab09f36ccc3b1dbe0c
Radu Dita <hades2510@yahoo.com>
2016-01-08 11:24:54 +0200

    added context

---
+++ UberMenu/Utils/UBContext.swift
@@ -0,0 +1,17 @@
+//
+//  UBContext.swift
+//  UberMenu
+//
+//  Created by Mac on 08/01/16.
+//  Copyright © 2016 samuraibonzai. All rights reserved.
+//
+
+import Foundation
+
+class UBContext{
+    
+    //singleton instance
+    static let sharedInstance = UBContext()
+    
+    var menu:Menu!
+}
