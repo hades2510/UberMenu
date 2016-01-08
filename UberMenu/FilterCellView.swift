7b83318e4c58e01efa1b2752503193d9b1dd764f
Radu Dita <hades2510@yahoo.com>
2016-01-08 11:13:45 +0200

    added filter view controller/cell

---
+++ UberMenu/FilterCellView.swift
@@ -0,0 +1,15 @@
+//
+//  FilterCellView.swift
+//  UberMenu
+//
+//  Created by Mac on 08/01/16.
+//  Copyright © 2016 samuraibonzai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FilterCellView: UITableViewCell{
+    
+    @IBOutlet weak var tagLabel: UILabel!
+}
---
+++ UberMenu/FilterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FilterViewController.swift
+//  UberMenu
+//
+//  Created by Mac on 07/01/16.
+//  Copyright © 2016 samuraibonzai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FilterViewController: UITableViewController{
+    
+}
