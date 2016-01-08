5e347b37fac50f70d2daf671cc7960e9ae1f4a25
Radu Dita <hades2510@yahoo.com>
2016-01-08 11:33:12 +0200

    presenting the available tags

--- UberMenu/FilterViewController.swift
+++ UberMenu/FilterViewController.swift
@@ -11,4 +11,18 @@ import UIKit
 
 class FilterViewController: UITableViewController{
     
+    private let cellID:String = "filterCellViewId"
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return UBContext.sharedInstance.menu.tags.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellID) as! FilterCellView
+        
+        cell.tagLabel.text = UBContext.sharedInstance.menu.tags[indexPath.row].name
+        
+        return cell
+    }
+    
 }
